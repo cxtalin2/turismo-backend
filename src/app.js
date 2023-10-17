c2483ea7cb659dd1cfffd4c710960cdd390ba73f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-10-17 18:57:05 -0500

    [+] app.js: Lanza servidor en puerto 3000 usando Express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log( 'Ecommerce' );
+const express = require( 'express' );
+const app = express();
+
+app.listen( 3000, function() {
+    console.log( `Servidor escuchando en http://localhost:3000` );
+} );
+
