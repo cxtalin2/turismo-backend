8a0cdc94fa59ca0bd5d145de0f5d3958d3158c1c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-10-17 19:49:47 -0500

    [+] products.routes: Define archivo de rutas para productos

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+/** Establece Rutas de la aplicacion */
+app.use(
+    '/api/products',                        // Middleware que configura la ruta
+    require( './routes/products.routes' )   // Importa el archivo de rutas para esta ruta '/api/products'
+);    
+
 dbConection();
 
 app.listen( PORT, function() {
---
+++ src/routes/products.routes.js
@@ -0,0 +1,6 @@
+const { Router } = require( 'express' );    // Importa el Router de Express
+const router = Router();                    // Invoca el Router de Express
+
+console.log( 'Router de Productos' );
+
+module.exports = router;                    // Expone el router para que sea usado por otros archivos
