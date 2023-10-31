06be762b540fa63ed2e5a6b74049f098c9c9b942
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-10-30 21:20:27 -0500

    [+] auth(controller/service): Registra usuario

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,39 +1,30 @@
-const { genSaltSync, hashSync } = require( 'bcrypt' );
-
 const UserModel = require("../models/User");
+
+const { findUserByUsername, registerUser } = require('../services/auth.service');
 
 const register = async ( req, res ) => {
     console.log( 'REGISTRANDO...' )
 
     // req.body: { username: '', password: '', role: '' }
-    const { username, password } = req.body;        // Equivale a: const inputData = req.body;  inputData.username
+    const { username } = req.body;        // Equivale a: const inputData = req.body;  inputData.username
 
     // 1. Verificar si existe el usuario --> username (email) 
-    // TODO: Poner en el SERVICIO
-    const userFound = await UserModel.findOne({ username });         // Equivale a: UserModel.find({ username: username });  
+    const userFound = await findUserByUsername( username );         // Equivale a: UserModel.find({ username: username });  
     
     if( userFound ) {
-        res.json({
+        res.status( 200 ).json({
             ok: false,
-            msg: 'El usuario ya existe!',
-            userFound
+            msg: 'El usuario ya existe!'
         });
     }
     
-    // Si no existe. Creamos el usuario
-    const dbUser = new UserModel( req.body );
-
-    // Encriptar la contrasenia
-    const salt = genSaltSync();                 
-    dbUser.password = hashSync( password, salt );
-
-    dbUser.save();
+    registerUser( req.body );
 
     // (Opcional). Generar JSONWebToken (Si y solo si queremos que al registrarse ingrese al sistema )
     // Responder al cliente 
-    res.json({
-        msg: 'Login de Usuario',
-        pass: dbUser
+    res.status( 201 ).json({
+        ok: true,
+        msg: 'Usuario registrado exitosamente'
     });
 }
 
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,8 +1,25 @@
-function registerUser () {
+const { genSaltSync, hashSync } = require( 'bcrypt' );
 
+const UserModel = require("../models/User");
+
+function registerUser ( newUser ) {
+    // Si no existe. Creamos el usuario
+    const dbUser = new UserModel( newUser );
+
+    // Encriptar la contrasenia
+    const salt = genSaltSync();                 
+    dbUser.password = hashSync( newUser.password, salt );
+
+    dbUser.save();
+}
+
+async function findUserByUsername( username ) {
+
+    return await UserModel.findOne({ username });         // Equivale a: UserModel.find({ username: username });  
 }
 
 
 module.exports = {
-    registerUser
+    registerUser,
+    findUserByUsername
 }
